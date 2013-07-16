f900d6d8d1510d27bc10d8a678398a35614ce341
Ray Wenderlich <ray.wenderlich@gmail.com>
2013-07-16 01:43:52 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -3,10 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var buffer = fs.readFileSync('index.html');
+    var string = buffer.toString();
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(string);
+    response.end();
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
